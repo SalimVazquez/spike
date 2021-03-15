b1b833638ab40f1abfe7a0bb0356f4dd51be94df
RocioValedoStgo <181098@ids.upchiapas.edu.mx>
2021-03-14 18:47:57 -0600

    Added Login View

---
+++ lib/src/pages/login.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        constraints: BoxConstraints.expand(),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background.jpg'),
+              fit: BoxFit.cover),
+        ),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.topLeft,
+                  padding: EdgeInsets.all(20.0),
+                  child: Image.asset('assets/images/logoSpike.png'),
+                  height: 110.0,
+                ),
+                Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  margin: EdgeInsets.all(15),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        padding: EdgeInsets.all(20.0),
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Sing In',
+                          style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromRGBO(38, 193, 101, 1)),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              labelText: 'Please enter your email'),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              labelText: 'Please enter your password'),
+                          obscureText: true,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: SizedBox(
+                          height: 50,
+                          width: 400,
+                          child: RaisedButton(
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(5.0)),
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/dashboard');
+                            },
+                            textColor: Colors.white,
+                            child: Text(
+                              'Submit',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            color: Color.fromRGBO(38, 193, 101, 1),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: SizedBox(
+                    height: 50,
+                    width: 400,
+                    child: RaisedButton(
+                      shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(5.0)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/register');
+                      },
+                      textColor: Color.fromRGBO(38, 193, 101, 1),
+                      child: Text(
+                        'Create an account',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
