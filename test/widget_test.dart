644a1cd191dc45679d09d66561d4b1d00c2db055
Uriel Ortiz <uortizfonseca@gmail.com>
2021-03-15 23:59:51 -0600

    Test login

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,21 +10,25 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:spike/src/pages/login.dart';
 
+Widget testWidgetLogin = new MediaQuery(
+    data: new MediaQueryData(),
+    child: new MaterialApp(
+      home: Login(),
+    ));
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(Login());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  group('Home Page Login Widget Test', () {
+    testWidgets('Sing in', (WidgetTester tester) async {
+      await tester.pumpWidget(testWidgetLogin);
+      expect(find.text('Sing In'), findsOneWidget);
+    });
+    testWidgets('Submit', (WidgetTester tester) async {
+      await tester.pumpWidget(testWidgetLogin);
+      expect(find.text('Submit'), findsOneWidget);
+    });
+    testWidgets('Create an account', (WidgetTester tester) async {
+      await tester.pumpWidget(testWidgetLogin);
+      expect(find.text('Create an account'), findsOneWidget);
+    });
   });
 }
